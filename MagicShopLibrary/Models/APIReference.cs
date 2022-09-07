25860131abc0a39a15ceec8b66b7a9cf2fe0c6bb
Chris Rancourt <rancourtvideo@gmail.com>
2022-09-07 11:14:59 -0400

    Getting Name now responds with ItemType

--- MagicShopLibrary/Models/APIReference.cs
+++ MagicShopLibrary/Models/APIReference.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,8 +8,45 @@ namespace MagicShopLibrary.Models
     public class APIReference
     {
         public string Index { get; set; }
-        public string Name { get; set; }
+
+        [JsonProperty("name")]
+        public string NameString { get; set; }
+
+        public ItemType Name
+        {
+            get { return GetCategoryEnum(NameString); }
+            private set { }
+        }
+
         public string URL { get; set; }
 
+        private ItemType GetCategoryEnum(string name)
+        {
+            switch (name)
+            {
+                case "Armor":
+                    return ItemType.Armor;
+                case "Ammunition":
+                    return ItemType.Ammunition;
+                case "Wonderous Item":
+                    return ItemType.WondrousItem;
+                case "Weapon":
+                    return ItemType.Weapon;
+                case "Rod":
+                    return ItemType.Rod;
+                case "Potion":
+                    return ItemType.Potion;
+                case "Ring":
+                    return ItemType.Ring;
+                case "Scroll":
+                    return ItemType.Scroll;
+                case "Staff":
+                    return ItemType.Staff;
+                case "Wand":
+                    return ItemType.Wand;
+                default:
+                    throw new Exception("Did not recognize Item type");
+            }
+        }
     }
 }
